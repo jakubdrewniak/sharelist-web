a63d4ec06300675af77494cd543092bf0886337a
jakubdrewniak <drewniak.jakub@gmail.com>
2022-03-17 13:01:38 +0100

    Client websocket POC

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core'
+import { Component, OnInit } from '@angular/core'
+import { Socket } from 'ngx-socket-io'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'sharelist-web'
+  callbackMessage = ''
+  messageFromServer = ''
+  constructor(private socket: Socket) {}
+
+  ngOnInit() {
+    this.socket
+      .fromEvent<{ msg: string }>('message')
+      .subscribe((data: { msg: string }) => {
+        this.messageFromServer = data.msg
+      })
+
+    this.socket.emit(
+      'join',
+      { msg: 'This is first message from client!' },
+      () => {
+        this.callbackMessage = 'Callback succeeded!'
+      }
+    )
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core'
+import { AppComponent } from './app.component'
+import { SocketIoConfig, SocketIoModule } from 'ngx-socket-io'
 import { BrowserModule } from '@angular/platform-browser'
 
-import { AppComponent } from './app.component'
+const config: SocketIoConfig = { url: 'http://localhost:8000', options: {} }
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, SocketIoModule.forRoot(config)],
   providers: [],
   bootstrap: [AppComponent],
 })
