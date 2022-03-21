ca90113f0aedf9ff925cf983d5312988af48b078
jakubdrewniak <drewniak.jakub@gmail.com>
2022-03-21 08:29:41 +0100

    List of catalogs styling

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -4,9 +4,10 @@ import { HomeComponent } from './home.component'
 import { RouterModule } from '@angular/router'
 import { MaterialModule } from '../material.module'
 import { CatalogsListComponent } from './catalogs-list/catalogs-list.component'
+import { SharedModule } from '../shared/shared.module'
 
 @NgModule({
   declarations: [HomeComponent, CatalogsListComponent],
-  imports: [CommonModule, RouterModule, MaterialModule],
+  imports: [CommonModule, RouterModule, MaterialModule, SharedModule],
 })
 export class HomeModule {}
