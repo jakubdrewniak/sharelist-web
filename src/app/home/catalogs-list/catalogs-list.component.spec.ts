9044f5f227178c7827c5dc0ff2aab0a39a2c3284
jakubdrewniak <drewniak.jakub@gmail.com>
2022-03-21 11:13:08 +0100

    Catalogs list render test

--- src/app/home/catalogs-list/catalogs-list.component.spec.ts
+++ src/app/home/catalogs-list/catalogs-list.component.spec.ts
@@ -1,18 +1,26 @@
+import { DebugElement } from '@angular/core'
 import { ComponentFixture, TestBed } from '@angular/core/testing'
+import { By } from '@angular/platform-browser'
 import { of } from 'rxjs/internal/observable/of'
-import { CatalogsListService } from '../services/catalogs.service'
+import { Catalog, CatalogsListService } from '../services/catalogs.service'
 
 import { CatalogsListComponent } from './catalogs-list.component'
 
 describe('CatalogsListComponent', () => {
   let component: CatalogsListComponent
   let fixture: ComponentFixture<CatalogsListComponent>
+  let el: DebugElement
 
-  let mockCatalogsSvc
+  let mockCatalogsSvc: jasmine.SpyObj<CatalogsListService>
+  const mockData: Catalog[] = [
+    { name: 'catalog1' },
+    { name: 'catalog2' },
+    { name: 'catalog3' },
+  ]
 
   beforeEach(async () => {
     mockCatalogsSvc = jasmine.createSpyObj(['getCatalogsList'])
-    mockCatalogsSvc.getCatalogsList.and.returnValue(of([]))
+    mockCatalogsSvc.getCatalogsList.and.returnValue(of(mockData))
 
     await TestBed.configureTestingModule({
       declarations: [CatalogsListComponent],
@@ -23,10 +31,19 @@ describe('CatalogsListComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(CatalogsListComponent)
     component = fixture.componentInstance
+    el = fixture.debugElement
     fixture.detectChanges()
   })
 
   it('should create', () => {
     expect(component).toBeTruthy()
+  })
+
+  it('should render list of catalogs', () => {
+    mockCatalogsSvc.getCatalogsList.and.returnValue(of(mockData))
+    fixture.detectChanges()
+    const catalogsList = el.queryAll(By.css('mat-list-item'))
+
+    expect(catalogsList.length).toBe(mockData.length)
   })
 })
